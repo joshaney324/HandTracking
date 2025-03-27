37a4473ea45c82ef7253bd8954f2a2b16542a0e0
joshaney324 <josh.aney@icloud.com>
2025-03-27 11:53:54 -0600

    Add hyperparameter tuning

---
+++ src/model.py
@@ -0,0 +1,49 @@
+import keras_tuner as kt
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+def build_model(input_shape, output_shape):
+    def model_builder(hp):
+        model = keras.Sequential()
+        model.add(layers.Input(shape=(input_shape,)))
+
+        hp_units = hp.Int('units', min_value=32, max_value=512, step=32)
+        model.add(layers.Dense(units=hp_units, activation='relu'))
+
+        for i in range(hp.Int('num_layers', 1, 3)):
+            model.add(layers.Dense(units=hp.Int(f'units_{i}', 32, 512, step=32), activation='relu'))
+
+        model.add(layers.Dense(output_shape, activation='softmax'))  # Output layer
+
+        hp_learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])
+        model.compile(
+            optimizer=keras.optimizers.Adam(learning_rate=hp_learning_rate),
+            loss='categorical_crossentropy',
+            metrics=['accuracy']
+        )
+
+        return model
+
+    return model_builder
+
+
+def hyperparameter_search(X_train, y_train, X_test, y_test, max_trials):
+    tuner = kt.RandomSearch(
+        build_model(len(X_train[0]), len(y_train[0])),
+        objective='val_accuracy',
+        max_trials=max_trials,
+        executions_per_trial=1,
+        directory='tuning_dir',
+        project_name='dense_model_tuning'
+    )
+
+    tuner.search(X_train, y_train, epochs=20, validation_data=(X_test, y_test), verbose=1)
+
+    best_model = tuner.get_best_models(num_models=1)[0]
+    return best_model
+
+
+def complete_model(X_train, y_train, X_test, y_test, max_trials):
+    model = hyperparameter_search(X_train, y_train, X_test, y_test, max_trials)
+    return model
--- src/real_time_model.py
+++ src/real_time_model.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
+from model import complete_model
 import numpy as np
 from sklearn.model_selection import train_test_split
 import cv2
@@ -7,30 +7,12 @@ import mediapipe as mp
 data = np.loadtxt('../data/data.csv', delimiter=",")
 np.random.shuffle(data)
 
-
 datapoints = data[:, :-5]
 labels = data[:, -5:]
 
-loss_fn = 'categorical_crossentropy' if labels.shape[1] > 1 else 'sparse_categorical_crossentropy'
-
 X_train, X_test, y_train, y_test = train_test_split(datapoints, labels, test_size=0.2, random_state=42)
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(256, activation='relu', input_shape=(datapoints.shape[1],)),
-    tf.keras.layers.Dropout(0.3),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.3),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(5, activation='softmax')
-])
-
-model.compile(optimizer='adam',
-              loss=loss_fn,
-              metrics=['accuracy'])
-
-model.summary()
-
-model.fit(X_train, y_train, epochs=25, batch_size=32, validation_data=(X_test, y_test))
+model = complete_model(X_train, y_train, X_test, y_test, 3)
 
 test_loss, test_acc = model.evaluate(X_test, y_test)
 print(f"Test Accuracy: {test_acc:.4f}")
